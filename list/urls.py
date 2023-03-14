2d08c662b956baad863daf03b8fe861273ca0db6
Monica Murray <Monica.rickerby@gmail.com>
2023-03-14 21:55:00 +0000

    Add Delete feature, Delete class-based View, add template and connect urls.  Test function.

--- list/urls.py
+++ list/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.EntryList.as_view(), name='home'),
     path('add/', views.AddView.as_view(), name='add'),
     path('edit/<int:pk>/', views.EditView.as_view(), name='edit'),
+    path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
 ]
--- list/views.py
+++ list/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import generic, View
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Entry
 from .forms import NoteForm
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 # Create your views here.
@@ -22,23 +23,50 @@ class EntryList(generic.ListView):
 
     #     return render(request, 'shopping-list/templates/index.html', context)
 
+
 class AddView(CreateView):
     # CRUD - Create a new shopping list entry using the fields from Entry model
-    
+
     model = Entry
     # fields = []
     template_name = 'add_to_list.html'
     fields = '__all__'
+    
     success_url = reverse_lazy('home')
+
+    # Next, add a success flash message when form is submitted.  
+    # should I be able to add a post method to this class-based view?
+
+    # messages.add_message(request, messages.INFO, 'Hello world.')
+    # 
+    # Not working - first 'request' is an error, 
+    # def post(self, request, *args, **kwargs):
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         messages.add_message(messages.INFO, 'Hello world.')
+    #         # <process form cleaned data>
+    #         return HttpResponseRedirect('/success/')
+
+    #     return render(request, self.template_name, {'form': form})
+    
 
 
 class EditView(UpdateView):
     # CRUD - Edit shopping list entry using the entry's primary key
-    
+
     model = Entry
     # fields = []
     template_name = 'edit_entry.html'
     fields = '__all__'
     pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('home')
+
+
+class Delete(DeleteView):
+    # CRUD - Target the entry with the pk again
+
+    model = Entry
+    pk_url_kwarg = 'pk'
+    template_name = 'delete.html'
     success_url = reverse_lazy('home')
 
